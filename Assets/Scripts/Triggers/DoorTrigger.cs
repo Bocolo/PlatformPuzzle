f9512d7b0847ea201d46427b10269ee1d9d9a042
bocolo <bronaghcolt@hotmail.com>
2021-05-15 22:13:29 +0100

    updating door functionality

--- Assets/Scripts/Triggers/DoorTrigger.cs
+++ Assets/Scripts/Triggers/DoorTrigger.cs
@@ -9,30 +9,63 @@ namespace Platformer.Trigger
     public class DoorTrigger : TriggerableObject //MonoBehaviour
     {
 
+        [SerializeField] Vector2 locationToMoveTo;
+        [SerializeField] float speed;
+        Vector3 originalLocation;
         [SerializeField]Lever thisObjectsTrigger;
         [SerializeField] bool hasBeenTriggered;
+        [SerializeField] bool hasReachedTop;
+        [SerializeField] bool hasReachedBottom =true;
+        private void Start()
+        {
+            originalLocation = transform.position;
+        }
         private void Update()
         {
             if (thisObjectsTrigger.isTriggered && !hasBeenTriggered)
             {
-                ActivateByTrigger();
+               
                 hasBeenTriggered = true;
             }
             if(!thisObjectsTrigger.isTriggered && hasBeenTriggered)
             {
-                DeActivateByTrigger();
+              
                 hasBeenTriggered = false;
+            }
+            if (hasBeenTriggered)
+            {
+                if (!hasReachedTop)
+                {
+                    ActivateByTrigger();
+                }
+            }
+            else
+            {
+                if (!hasReachedBottom)
+                {
+                    DeActivateByTrigger();
+                }
             }
         }
         public override void ActivateByTrigger()
         {
-          //  base.ActivateByTrigger();
-            Debug.Log("Activating Door");
+            hasReachedBottom = false;
+            Vector3 newPosition = new Vector3(transform.position.x, locationToMoveTo.y, transform.position.z);
+            transform.position = Vector2.MoveTowards(transform.position, newPosition,
+                 speed * Time.deltaTime);
+            if(transform.position == newPosition)
+            {
+                hasReachedTop = true;
+            }         
         }
         public override void DeActivateByTrigger()
         {
-            Debug.Log("DeActivating Door");
-          //  base.DeActivateByTrigger();
+            hasReachedTop = false;
+            transform.position = Vector2.MoveTowards(transform.position, originalLocation, speed * Time.deltaTime);
+            if (transform.position == originalLocation)
+            {
+                hasReachedBottom = true;
+            }
         }
     }
 }
